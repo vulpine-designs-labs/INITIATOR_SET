197e1daef59fd5e96f5f25e4e128572086a95cd9
Jason Alexander <jasonalexander.dev@gmail.com>
2022-02-04 05:54:10 +0000

    Cleared main object

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,16 +5,4 @@ import utils.kozak._
 
 object main extends App {
 	
-	val kzNucleotide = new KzNucleotide(
-		A = 0.7,
-		U = 0.7,
-		G = 0.2,
-		C = 0.2,
-		Importance = 1
-	)
-	
-	val kzNucleotideStr: String = kzNucleotide
-	
-	println(kzNucleotideStr)
-	
 }
